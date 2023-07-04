abc774cd6b6e50df8c3c309d8073d55ed18c3791
Anthony Buisset <abuisset@gmail.com>
2023-07-04 14:30:47 +0200

    add tech definitions

--- scripts/llm/src/index.ts
+++ scripts/llm/src/index.ts
@@ -2,10 +2,10 @@ import dotenv from 'dotenv';
 import { Repo } from "./github";
 import { Options } from "./options";
 import ora from 'ora';
-import { getRepoOverview } from "./llm";
+import { explainTechnicalTerms, getRepoOverview } from "./llm";
+import fs from 'fs';
 
 dotenv.config();
-
 
 async function main() {
     const options: Options = {
@@ -14,10 +14,20 @@ async function main() {
 
     const [owner, repo] = process.argv[2].split("/");
     options.spinner?.start(`[${owner}/${repo}]`);
-    const response = await getRepoOverview(new Repo(owner, repo), options);
-    options.spinner?.succeed();
 
-    console.log(response);
+    const purpose = await getRepoOverview(new Repo(owner, repo), options);
+
+    fs.writeFileSync("project_description.md", `
+# ${repo}
+
+## Purpose
+${purpose}
+
+## Definitions
+${await explainTechnicalTerms(purpose, options)}
+`);
+
+    options.spinner?.succeed();
 }
 
 
--- scripts/llm/src/llm.ts
+++ scripts/llm/src/llm.ts
@@ -1,16 +1,18 @@
 import { OpenAI } from "langchain/llms/openai";
-import { ConversationChain } from "langchain/chains";
+import { ConversationChain, LLMChain } from "langchain/chains";
 import { BufferMemory } from "langchain/memory";
 import { PromptTemplate } from "langchain/prompts";
 import { Repo } from "./github";
 import { Options } from "./options";
+import { SerpAPI } from "langchain/tools";
+import { initializeAgentExecutorWithOptions } from "langchain/agents";
 
 export async function getRepoOverview(repo: Repo, { spinner }: Options) {
-    const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9, modelName: "gpt-4" });
+    const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.3, modelName: "gpt-4" });
 
     const prompt = new PromptTemplate({
         template: `
-        Tell me the purpose of the {owner}/{name} github repository based on its README file and Github description.
+        Explain the purpose of the {owner}/{name} github repository based on its README file and Github description.
         The github description is: "{description}".
         The content of the README file is:
 
@@ -29,7 +31,33 @@ export async function getRepoOverview(repo: Repo, { spinner }: Options) {
     await chain.call({ input: await prompt.format({ owner: repo.owner, name: repo.name, description: repoDetails ? repoDetails.description : "", readme: repoReadme }) });
 
     spinner.suffixText = "Refining the purpose";
-    const { response } = await chain.call({ input: "Give me a summary of your answer in less than 300 characters. Write the result in markdown format." });
+    const { response } = await chain.call({ input: "Give me a summary of your answer in less than 300 characters. Do not mention the repository name in the response. Write the result in markdown format." });
 
     return response;
+}
+
+
+export async function explainTechnicalTerms(overview:string, { spinner }: Options) {
+    spinner.suffixText = "Explaining technical terms";
+
+    const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.3, modelName: "gpt-3.5-turbo" });
+
+    const tools = [new SerpAPI(process.env.SERP_API_KEY)];
+
+    const executor = await initializeAgentExecutorWithOptions(tools, model, {
+        agentType: "zero-shot-react-description",
+        verbose: !!process.env.DEBUG
+    });
+
+    const prompt = new PromptTemplate({
+        template: `Define the complex technical terms of the following overview: {overview}.
+        Reply only with the list of terms and their definitions in markdown format.
+        Add links to internet resources to get more details on each term.
+        `,
+        inputVariables: ["overview"],
+    });
+
+    const { output } = await executor.call({ input: await prompt.format({overview}) });
+
+    return output;
 }
