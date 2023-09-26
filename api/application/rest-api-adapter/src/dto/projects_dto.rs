9ed6b64edfbb9d2bebdfe20e8241b4f37c53fe3e
PierreOucif <pierre.oucif@gadz.org>
2023-09-26 10:44:42 +0200

    Update dependencies

--- api/application/rest-api-adapter/src/dto/projects_dto.rs
+++ api/application/rest-api-adapter/src/dto/projects_dto.rs
@@ -1,6 +1,7 @@
-use domain::{BudgetId, DomainError, ProjectId, ProjectVisibility};
+use domain::{BudgetId, ProjectId, ProjectVisibility, sponsor};
 use url::Url;
 use serde::{Deserialize, Serialize};
+use rust_decimal::Decimal;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -17,8 +18,16 @@ pub struct PostProjectRequest {
     long_description: String,
     telegram_link: Option<Url>,
     logo_url: Option<Url>,
-    initial_budget: Option<dto::Allocation>,
+    initial_budget: Option<Allocation>,
     hiring: Option<bool>,
     rank: Option<i32>,
     visibility: Option<ProjectVisibility>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Allocation {
+    amount: Decimal,
+    currency: String,
+    sponsor: Option<sponsor::Id>,
 }
