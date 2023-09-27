7237657c0d2a166a994cb13fe01a42cc037fda7e
Anthony Buisset <abuisset@gmail.com>
2023-09-27 09:25:04 +0200

    use responder derive

--- api/application/rest-api-adapter/src/error/mod.rs
+++ api/application/rest-api-adapter/src/error/mod.rs
@@ -2,19 +2,10 @@ use api_domain::ports::{input::project_facade, output::project_storage};
 use derive_more::From;
 use http_api_problem::HttpApiProblem;
 use reqwest::StatusCode;
-use rocket::{
-	request::Request,
-	response::{Responder, Result},
-};
+use rocket::response::Responder;
 
-#[derive(Debug, From)]
+#[derive(Debug, From, Responder)]
 pub struct ApiError(HttpApiProblem);
-
-impl<'r, 'o: 'r> Responder<'r, 'o> for ApiError {
-	fn respond_to(self, request: &'r Request<'_>) -> Result<'o> {
-		self.0.respond_to(request)
-	}
-}
 
 impl From<project_facade::Error> for ApiError {
 	fn from(value: project_facade::Error) -> Self {
