e65d9b8894449711568299912f38a81f71b0d2a4
Anthony Buisset <abuisset@gmail.com>
2023-09-29 17:19:02 +0200

    add other simple crawlers

--- new-github-indexer/domain/src/ports/output/github_api/repo.rs
+++ new-github-indexer/domain/src/ports/output/github_api/repo.rs
@@ -1,7 +1,12 @@
 use super::Result;
-use crate::models::Repository;
+use crate::models::*;
 
 #[async_trait]
 pub trait Port: Send + Sync {
-	async fn repo_by_id(&self, repo_id: u64) -> Result<Repository>;
+	async fn repo_by_id(&self, repo_id: RepositoryId) -> Result<Repository>;
+
+	async fn repo_by_owner_name(&self, repo_owner: String, repo_name: String)
+	-> Result<Repository>;
+
+	async fn repo_languages_by_id(&self, repo_id: RepositoryId) -> Result<Languages>;
 }
--- new-github-indexer/infrastructure/src/adapters/github_api/adapters/repo.rs
+++ new-github-indexer/infrastructure/src/adapters/github_api/adapters/repo.rs
@@ -4,11 +4,23 @@ use crate::adapters::github_api::Client;
 
 #[async_trait]
 impl repo::Port for Client {
-	async fn repo_by_id(&self, repo_id: u64) -> Result<Repository> {
-		let repo: octocrab_indexer::models::Repository =
-			self.get_as(format!("/repositories/{repo_id}")).await?;
-		let languages: Languages =
-			self.get_as(format!("/repositories/{repo_id}/languages")).await?;
-		Ok(Repository { repo, languages })
+	async fn repo_by_id(&self, repo_id: RepositoryId) -> Result<Repository> {
+		self.get_as(format!("/repositories/{repo_id}")).await.map_err(Into::into)
+	}
+
+	async fn repo_by_owner_name(
+		&self,
+		repo_owner: String,
+		repo_name: String,
+	) -> Result<Repository> {
+		self.get_as(format!("/repos/{repo_owner}/{repo_name}"))
+			.await
+			.map_err(Into::into)
+	}
+
+	async fn repo_languages_by_id(&self, repo_id: RepositoryId) -> Result<Languages> {
+		self.get_as(format!("/repositories/{repo_id}/languages"))
+			.await
+			.map_err(Into::into)
 	}
 }
