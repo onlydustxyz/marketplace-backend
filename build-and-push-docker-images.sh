2e6eabb4e3f6ab914c421584d82416e1a6e6a297
PierreOucif <pierre.oucif@gadz.org>
2023-10-16 14:09:52 +0200

    Add api and indexer build and push

--- build-and-push-docker-images.sh
+++ build-and-push-docker-images.sh
@@ -2,6 +2,6 @@
 set +x;
 
 aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin 113659546096.dkr.ecr.eu-west-3.amazonaws.com
-#docker build -t marketplace-backend-api . --build-arg BIN_NAME=api
-#docker tag marketplace-backend-api:latest 113659546096.dkr.ecr.eu-west-3.amazonaws.com/marketplace-backend-api:latest
-#docker push 113659546096.dkr.ecr.eu-west-3.amazonaws.com/marketplace-backend-api:latest
+docker build -t marketplace-backend-$1 . --build-arg BIN_NAME=$1
+docker tag marketplace-backend-$1:latest 113659546096.dkr.ecr.eu-west-3.amazonaws.com/marketplace-backend-$1:latest
+docker push 113659546096.dkr.ecr.eu-west-3.amazonaws.com/marketplace-backend-$1:latest
