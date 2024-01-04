39c030398ea69a5b9f830ddad0549b5e240ab2c5
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-01-04 13:39:36 +0100

    [B-1309] Remove IBAN validation (#41)

--- common/domain/src/value_objects/iban.rs
+++ common/domain/src/value_objects/iban.rs
@@ -1,16 +1,7 @@
-use std::fmt::Display;
-
-use derive_more::{From, FromStr, Into};
-use iban::IbanLike;
+use derive_more::{Display, From, FromStr, Into};
 use serde_with::{DeserializeFromStr, SerializeDisplay};
 
 #[derive(
-	Debug, Clone, PartialEq, Eq, From, Into, FromStr, SerializeDisplay, DeserializeFromStr,
+	Debug, Clone, PartialEq, Eq, From, Into, FromStr, Display, SerializeDisplay, DeserializeFromStr,
 )]
-pub struct Iban(iban::Iban);
-
-impl Display for Iban {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "{}", self.0.electronic_str())
-	}
-}
+pub struct Iban(String);
